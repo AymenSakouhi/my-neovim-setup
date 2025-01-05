f3c88e74b10cc8774679f47db3e789292fe328a0
Aymen Sakouhi <asakouhi@Mac.local>
2025-01-05 18:34:52 +0100

    adding macos config

--- config.fish
+++ config.fish
@@ -23,3 +23,37 @@ end
 
 # Created by `pipx` on 2024-12-25 15:35:46
 set PATH $PATH /home/rod/.local/bin
+
+
+# MAC OS CONFIG
+# Under this line all is for macos config
+# if status is-interactive
+#     # Commands to run in interactive sessions can go here
+# end
+#
+# if status is-interactive
+#     # Set up Homebrew
+#     if test -f /opt/homebrew/bin/brew
+#         eval (/opt/homebrew/bin/brew shellenv)
+#     end
+#     # Initialize oh-my-posh
+#     oh-my-posh init fish --config $HOME/.poshthemes/ys.omp.json | source
+# end
+#
+# # Path configurations
+# set -x PATH $PATH /usr/local/bin /opt/homebrew/bin
+#
+# # NVM setup for fish
+# set -x NVM_DIR "$HOME/.nvm"
+# if test -f /opt/homebrew/opt/nvm/nvm.sh
+#     bass source /opt/homebrew/opt/nvm/nvm.sh
+# end
+#
+# # FZF setup for fish
+# #if type -q fzf
+# #    fzf_key_bindings
+# #end
+#
+# # Created by `pipx` on 2024-12-25 15:35:46
+# set PATH $PATH $HOME/.local/bin
+#
