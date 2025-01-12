f05882dae3989aa87e04f462a5f099fb0dd279b6
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-01-12 02:18:01 +0100

    trying something out

--- lua/aymen/plugins/nvim-cmp.lua
+++ lua/aymen/plugins/nvim-cmp.lua
@@ -52,12 +52,12 @@ return {
 			}),
 
 			-- configure lspkind for vs-code like pictograms in completion menu
-			formatting = {
+			formatting = require({
 				format = lspkind.cmp_format({
 					maxwidth = 50,
 					ellipsis_char = "...",
 				}),
-			},
+			}),
 		})
 	end,
 }
