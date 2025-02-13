b99d63201f6ce51c8e753218a35132443d8abf0c
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-02-13 02:52:07 +0100

    adding zoxide

--- config.fish
+++ config.fish
@@ -31,6 +31,13 @@ alias ll='eza -al --group-directories-first'
 alias ls='eza -alF --color=always --sort=size | grep -v /'
 alias lt='eza -al --sort=modified'
 
+# Load zoxide using bass
+if not functions -q bass
+    curl -sL https://git.io/fisher | source && fisher install edc/bass
+end
+zoxide init fish | source
+
+
 # MAC OS CONFIG
 # Under this line all is for macos config
 # if status is-interactive
